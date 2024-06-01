17cbff26c096d4e8ef2124c128baffdbf8504208
vinicius lima <vinicius.limalveslinux@gmail.com>
2024-06-01 13:55:27 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/stockeeper/gestao_inventario/GestaoInventarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.stockeeper.gestao_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stockeeper/gestao_inventario/GestaoInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stockeeper.gestao_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
